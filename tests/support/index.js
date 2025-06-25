f3687e404ae0394fc6bb801f460d069357664111
JadsonCardoso <jadsonc190@gmail.com>
2025-06-25 19:44:29 -0300

    chore: Movendo pasta Actions para pasta SUPPORT

--- tests/support/index.js
+++ tests/support/index.js
@@ -1,11 +1,11 @@
 import { test as base, expect } from '@playwright/test'
 
 // Importando os arquivos
-import { Login } from '../actions/Login.js'
-import { MinhaConta } from '../actions/MinhaConta.js'
-import { Endereco } from '../actions/Endereco.js'
-import { Cadastro } from '../actions/Cadastro.js'
-import { Components } from '../actions/Components.js'
+import { Login } from './actions/Login.js'
+import { MinhaConta } from './actions/MinhaConta.js'
+import { Endereco } from './actions/Endereco.js'
+import { Cadastro } from './actions/Cadastro.js'
+import { Components } from './actions/Components.js'
 
 const test = base.extend({ 
     page: async ({ page }, use) => {
