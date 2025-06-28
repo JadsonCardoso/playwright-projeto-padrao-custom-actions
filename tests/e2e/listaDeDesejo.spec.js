82492660aac0265e0458ec950a928f7bf47cbb41
JadsonCardoso <jadsonc190@gmail.com>
2025-06-27 22:49:42 -0300

    test: Alteração de versão de produto. Adicionar produto pela tela de detalhes, e melhorias na função de validações

--- tests/e2e/listaDeDesejo.spec.js
+++ tests/e2e/listaDeDesejo.spec.js
@@ -14,7 +14,7 @@ test('Adicionado produtos no carrrinho', async ({ page }) => {
 
     await page.listaDeDesejos.acessarPaginaDeDesejos()
     await page.listaDeDesejos.adicionarProdutoCarrinho()
-    await page.listaDeDesejos.validarAdicaoDeProduto()
+    await page.listaDeDesejos.validarAdicaoDeProduto('$153.00')
 })
 
 test('Remover produto na lista de defeitos', async ({ page }) => {
@@ -30,11 +30,30 @@ test('Acessando produto adicionados no Carrinho', async ({ page }) => {
 
     await page.listaDeDesejos.acessarPaginaDeDesejos()
     await page.listaDeDesejos.adicionarProdutoCarrinho()
-    await page.listaDeDesejos.validarAdicaoDeProduto()
+    await page.listaDeDesejos.validarAdicaoDeProduto('$153.00')
 })
 
 test('Acessando lista de Desejos', async ({ page }) => {
     await page.login.fazerLogin(userEmail, '123456')
-    
+
     await page.listaDeDesejos.acessarListaDeDesejos()
+    await page.listaDeDesejos.acessarProdutoPelaListadeDesejos()
+})
+
+test('Alterando versão do produto', async ({ page }) => {
+    await page.login.fazerLogin(userEmail, '123456')
+
+    await page.listaDeDesejos.acessarListaDeDesejos()
+    await page.listaDeDesejos.acessarProdutoPelaListadeDesejos()
+    await page.listaDeDesejos.alterarVersaoDoProduto('small')
+})
+
+
+test('Adicionando produto no carrinho via tela de detalhes do produto', async ({ page }) => {
+    await page.login.fazerLogin(userEmail, '123456')
+
+    await page.listaDeDesejos.acessarListaDeDesejos()
+    await page.listaDeDesejos.acessarProdutoPelaListadeDesejos()
+    await page.listaDeDesejos.adicionarProdutoTelaDetalhes()
+    await page.listaDeDesejos.validarValorDoCarrinho('$153.00')
 })
--- tests/support/actions/ListasDeDesejos.js
+++ tests/support/actions/ListasDeDesejos.js
@@ -24,13 +24,43 @@ export class ListaDeDesejos {
         await removerProd.click()
     }
 
-    async validarAdicaoDeProduto() {
+
+    async acessarListaDeDesejos() {
+        await this.page.locator('a[href="#offcanvas-wishlish"] .item-count').nth(0).click()
+        await expect(this.page.getByText('View wishlist')).toBeVisible()
+    }
+
+    async acessarProdutoPelaListadeDesejos() {
+        await this.page.locator('a[href="/product-details-one/9"]').nth(1)
+            .getByText('Boho Tops for Girls').click()
+
+        await expect(this.page.locator('.customs_sel_box')).toBeVisible()
+
+    }
+
+    async alterarVersaoDoProduto(modelo) {
+        await this.page.locator('.customs_sel_box').selectOption({ value: modelo });
+    }
+
+    async adicionarProdutoTelaDetalhes() {
+        const addCart = await this.page.locator('a[class="theme-btn-one btn-black-overlay btn_sm"]')
+        addCart.click()
+        await this.validarMensagemDesucesso()
+    }
+
+    async validarMensagemDesucesso() {
         await expect(this.page.locator('#swal2-title')).toHaveText('Success!')
         await expect(this.page.locator('#swal2-html-container')).toHaveText('Successfully added to your Cart')
+    }
+
+    async validarAdicaoDeProduto(subtotal) {
+        await this.validarMensagemDesucesso()
         await expect(this.page.locator('.item-count').nth(1)).toHaveText('4')
         await this.page.locator('.item-count').nth(1).click()
+        await this.validarValorDoCarrinho(subtotal)
         await expect(this.page.locator('a[href="/product-details-one/9"]').nth(1)).toHaveText('Boho Tops for Girls')
     }
+
 
     async validarRemocaoProduto(quantidadeProd) {
         await expect(this.page.getByText('Skater Dress')).toBeHidden()
@@ -39,9 +69,8 @@ export class ListaDeDesejos {
         await expect(this.page.getByText('Skater Dress')).toBeHidden()
     }
 
-    async acessarListaDeDesejos() {
-        await this.page.locator('a[href="#offcanvas-wishlish"] .item-count').nth(0).click()
-        await expect(this.page.getByText('View wishlist')).toBeVisible()
-    }
 
+    async validarValorDoCarrinho(subtotal) {
+        await expect(this.page.locator('.offcanvas-cart-total-price-value')).toHaveText(subtotal)
+    }
 }
